2cfe94cab56bfa44c3e0d7bba570c9cceb331c59
mikailcatli09-sudo <mikailcatli09@gmail.com>
2026-05-15 08:59:55 +0800

    Update BookController.cs

--- CatliLibraryNowAPI/Controllers/BookController.cs
+++ CatliLibraryNowAPI/Controllers/BookController.cs
@@ -7,65 +7,63 @@ namespace CatliLibraryNowAPI.Controllers
 {
     [Route("api/v1/books")]
     [ApiController]
-    public class BookController : ControllerBase
+
+    public class BooksControllers : ControllerBase
     {
         private static List<Book> books = new List<Book>
-        {
-        new Book { Id = 1, Title = "Don Quiote", Author = "Lewis Carroll", Genre ="Fiction" , Available = true, PublishedYear = 1886},
-        new Book { Id = 2,  Title = "The adventures Lily", Author ="Mark Twain" , Genre ="Historical", Available = true,PublishedYear = 1935},
-        };
+       {
+           new Book { Id = 1, Title = "Theo Of Golden" , Author = "Allen Levi", Genre = "Contemporary Fiction" ,Available = true, PublishedYear = 2025 },
+           new Book { Id = 2, Title = "Theo " , Author = " Levi", Genre = " Fiction" ,Available = true, PublishedYear = 2020 }
+
+       };
 
         [HttpGet]
-
         public IActionResult GetAll()
         {
-            return Ok(new { status = "success", data = books, message = "Books retrieved." });
+            return Ok(new { status = "sucess", data = books, message = "Books retrieved" });
         }
-
         [HttpGet("{id}")]
-        public IActionResult Get(int id)
+        public IActionResult GetById(int id)
         {
             var book = books.FirstOrDefault(b => b.Id == id);
             if (book == null)
-                return NotFound(new { status = "error", data = (object?)null, message = "Books not found" });
-            return Ok(new { status = "success", data = book, message = "Book retrieved." });
-        }
 
-        [HttpPost]
-        public IActionResult Create([FromBody] Book newBook)
-        {
-            newBook.Id = books.Count + 1;
-            books.Add(newBook);
-            return CreatedAtAction(nameof(GetbyId), new { id = newBook.Id },
-                new { status = "success", data = newBook, message = "Book created." });
+                return NotFound(new { status = "error", data = (object?)null, message = "s not found" });
+            return Ok(new { status = "success", data = books, message = "Books Retrieved" });
+
         }
         [HttpPost("{id}")]
-
-        public IActionResult Update(int id, [FromBody] Book updateBook)
+        public IActionResult Create([FromBody] Book newbook)
+        {
+            newbook.Id = books.Count + 1;
+            books.Add(newbook);
+            return CreatedAtAction(nameof(GetById), new { id = newbook.Id },
+            new { status = "success", data = books, message = "Books retrieved" });
+        }
+        [HttpPut]
+        public IActionResult Update(int id, [FromBody] Book Updatebook)
         {
             var book = books.FirstOrDefault(b => b.Id == id);
             if (book == null)
-                return NotFound(new { status = "error", data = (object)null, message = "Book not found." });
-
-            book.Title = updateBook.Title;
-            book.Author = updateBook.Author;
-            book.Genre = updateBook.Genre;
-            book.Available = updateBook.Available;
-            book.PublishedYear = updateBook.PublishedYear;
-
-            return Ok(new { status = "success", data = book, message = "Book update." });
+                return NotFound(new { status = "error", data = (object?)null, messagae = "s not found." });
+            book.Title = Updatebook.Title;
+            book.Author = Updatebook.Author;
+            book.Genre = Updatebook.Genre;
+            book.Available = Updatebook.Available;
+            book.PublishedYear = Updatebook.PublishedYear;
+            return Ok(new { status = "success", data = books, message = "Books retrieved" });
 
         }
         [HttpDelete("{id}")]
-
         public IActionResult Delete(int id)
         {
             var book = books.FirstOrDefault(book => book.Id == id);
-            return NotFound(new { status = "error", data = (object?)null, message = "Book not Found" });
-
+            if (book == null)
+                return NotFound(new { status = "error", data = (object?)null, messagae = "s not found." });
             books.Remove(book);
-            return Ok(new { status = "success", date = (object?)null, message = "Books retrieved" });
+            return Ok(new { status = "success", data = books, message = "Books retrieved" });
+
+
         }
     }
 }
-
